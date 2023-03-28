700c964106991c2d964cfb09feffb695de2a7858
john109695 <64110752+john109695@users.noreply.github.com>
2023-03-28 13:38:31 -0400

    Updated code in factory script

--- Assets/Scripts/General/Factory/Factory.cs
+++ Assets/Scripts/General/Factory/Factory.cs
@@ -2,10 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Constants.Drop;
+using System;
 
 //TODO:: Add code for factory
 public static class Factory
 {
-    
+    public static GameObject getObject(DROPPABLES d)
+    {
+        GameObject gameObject= null;
+        switch (d)
+        {
+            case DROPPABLES.STALACTITE:
+                break;
+            case DROPPABLES.SHRINK:
+                break;
+            case DROPPABLES.SPEEDUP:
+                break;
+            case DROPPABLES.TIMEWARP_SLOW:
+                break;
+            case DROPPABLES.GROW:
+                break;
+            case DROPPABLES.SLOW:
+                break;
+            case DROPPABLES.TIMEWARP_FAST:
+                break;
+            default:
+                break;
+        }
+        return gameObject;
+    }
 }
 
