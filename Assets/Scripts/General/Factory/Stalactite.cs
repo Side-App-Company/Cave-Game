cb60fdf922fb86391bd893f324ccbb3ffd01b747
john109695 <64110752+john109695@users.noreply.github.com>
2023-03-28 23:57:10 -0400

    Changed base class to monobehaviour adding spawing of objects

--- Assets/Scripts/General/Factory/Stalactite.cs
+++ Assets/Scripts/General/Factory/Stalactite.cs
@@ -7,9 +7,16 @@ public class Stalactite : Droppable
 {
     public override DROPPABLES _name => DROPPABLES.STALACTITE;
     GameObject _stalactitePrefab;
+    Transform _stalactiteTransform;
+    private void Start()
+    {
+        _stalactitePrefab = gameObject;
+        _stalactiteTransform = _stalactitePrefab.GetComponent<Transform>();
+        _stalactiteTransform.localPosition = new Vector3(0, 20, 0);
+    }
     public override GameObject Do()
     {
         //Spawn triangle
-        return null;
+        return _stalactitePrefab;
     }
 }
