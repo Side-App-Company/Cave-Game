ad9b3b18170f97075685f6b958701bcc1e0a30f2
john109695 <64110752+john109695@users.noreply.github.com>
2023-03-28 22:11:03 -0400

    Added Factory House which will interface with the factory to spawn objects

--- Assets/Scripts/General/Factory/Factory.cs
+++ Assets/Scripts/General/Factory/Factory.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 using Constants.Drop;
 using System;
 
-//TODO:: Add code for factory
+//TODO:: 
 public static class Factory
 {
+    static GameObject _stalactitePrefab;
+    static GameObject _shrinkPrefab;
+    static GameObject _speedupPrefab;
+    static GameObject _timewarpSlowPrefab;
+    static GameObject _growPrefab;
+    static GameObject _slowPrefab;
+    static GameObject _timewarpFastPrefab;
+    static Vector2 _spawnPoint = new Vector2(0, 0);
+
     public static GameObject getObject(DROPPABLES d)
     {
         GameObject gameObject= null;
         switch (d)
         {
             case DROPPABLES.STALACTITE:
+                UnityEngine.Debug.Log("Stalactite");
                 break;
             case DROPPABLES.SHRINK:
+                UnityEngine.Debug.Log("Shrink");
                 break;
             case DROPPABLES.SPEEDUP:
+                UnityEngine.Debug.Log("Speedup");
                 break;
             case DROPPABLES.TIMEWARP_SLOW:
+                UnityEngine.Debug.Log("Time Warp Slow");
                 break;
             case DROPPABLES.GROW:
+                UnityEngine.Debug.Log("Grow");
                 break;
             case DROPPABLES.SLOW:
+                UnityEngine.Debug.Log("Slow");
                 break;
             case DROPPABLES.TIMEWARP_FAST:
+                UnityEngine.Debug.Log("Time Warp Fast");
                 break;
             default:
                 break;
