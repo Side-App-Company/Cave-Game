d2236c9bae60ad50124780d45431ceafff98764e
john109695 <64110752+john109695@users.noreply.github.com>
2023-03-30 22:22:37 -0400

    Updating factory

--- Assets/Scripts/General/Factory/Stalactite.cs
+++ Assets/Scripts/General/Factory/Stalactite.cs
@@ -1,6 +1,7 @@
 using Constants.Drop;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stalactite : Droppable
@@ -8,7 +9,7 @@ public class Stalactite : Droppable
     public override DROPPABLES _name => DROPPABLES.STALACTITE;
     GameObject _stalactitePrefab;
     Transform _stalactiteTransform;
-    private void Start()
+    private void Awake()
     {
         _stalactitePrefab = gameObject;
         _stalactiteTransform = _stalactitePrefab.GetComponent<Transform>();
@@ -16,7 +17,11 @@ public class Stalactite : Droppable
     }
     public override GameObject Do()
     {
+        if(_stalactitePrefab == null )
+            _stalactitePrefab = gameObject;
+        
         //Spawn triangle
+        Instantiate(_stalactitePrefab, _stalactiteTransform);
         return _stalactitePrefab;
     }
 }
