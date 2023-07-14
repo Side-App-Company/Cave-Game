3af564622f271bff4259d65ad6544e5a1557c336
imaginesto <imaginesto.art@gmail.com>
2023-07-14 00:36:55 -0400

    Big ART PUSH

---
+++ Assets/Art/Sprites/Environment/Kodeys_Secrets/Old/Oldest_Sheets/PSD_SpriteExporter.cs
@@ -0,0 +1,40 @@
+ 
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+ 
+public class ExportSlicedTexture : EditorWindow
+{
+    private Texture2D texture;
+ 
+    [MenuItem("Window/Export Sliced Texture")]
+    public static void ShowWindow()
+    {
+        GetWindow<ExportSlicedTexture>("Export Sliced Texture");
+    }
+ 
+    private void OnGUI()
+    {
+        texture = (Texture2D)EditorGUILayout.ObjectField(texture, typeof(Texture2D), allowSceneObjects: false);
+        if (GUILayout.Button("Generate Sliced Image") && texture != null)
+        {
+            SaveTextureAsPNG(texture);
+        }
+    }
+ 
+    public static void SaveTextureAsPNG(Texture2D texture)
+    {
+        byte[] _bytes = texture.EncodeToPNG();
+ 
+        var assetPath = AssetDatabase.GetAssetPath(texture);
+        var assetFileInfo = new FileInfo(assetPath);
+        var assetDirectory = assetFileInfo.Directory.FullName;
+        var fileName = assetFileInfo.Name;
+ 
+        var filePath = Path.Combine(assetDirectory, fileName + "_sliced.png");
+ 
+        File.WriteAllBytes(filePath, _bytes);
+        Debug.Log(_bytes.Length / 1024 + "Kb was saved as: " + filePath);
+    }
+}
+ 
